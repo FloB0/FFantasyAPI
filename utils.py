b98c61f4f712e1329bc62478eb7cbbc23b3ba746
Florian Wasmeire <wasmeierflo@gmail.com>
2023-09-10 23:48:54 +0200

    URL builder for League standings

--- utils.py
+++ utils.py
@@ -6,3 +6,8 @@ def getTimeFromTimestamp(timestamp):
     print(dt_object)
     formatted_date = dt_object.strftime('%Y-%m-%d %H:%M:%S')
     print(formatted_date)
+
+
+def getLeagueStandings(leagueId)
+    url = 'https://stage.api.fantasy.nfl.com/v3/leagues/{leagueId}/standings'.format(leagueId)
+    return url
